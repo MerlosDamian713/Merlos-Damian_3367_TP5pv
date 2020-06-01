65f6e6ac36ed91398b4dc619616a9c7a32ee86e2
Damian <Damian@DESKTOP-M74DGDL>
2020-06-01 16:23:54 -0300

    Asunto: Inicializacion de proyectos Responsable: Merlos Damian Tarea: Inicializando el proyecto

---
+++ src/main/java/ar/edu/unju/fi/Merlos3367Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Merlos3367Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Merlos3367Tp5Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/fi/Merlos3367Tp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Merlos3367Tp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
